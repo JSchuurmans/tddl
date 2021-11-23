d76b80d2a937bcc1304f217a32ee7732ef74f33e
JSchuurmans <jetzeschuurmans@gmail.com>
2021-11-23 14:22:34 +0100

    add scaled and relative error

--- src/tddl/utils/approximation.py
+++ src/tddl/utils/approximation.py
@@ -5,5 +5,35 @@ def calculate_error(
     approximation,
     **kwargs,
 ):
+    """
+    Returns:
+        The norm of the difference with tracking gradients.
+    """
     with torch.no_grad():
         return torch.norm(original-approximation, **kwargs)
+
+def calculate_scaled_error(
+    original, 
+    approximation,
+    **kwargs,
+):
+    """
+    Returns:
+        The error and scales with the number of elements in the original tensor.
+    """
+    with torch.no_grad():
+        return torch.norm(original-approximation, **kwargs) / torch.numel(original)
+
+
+
+def calculate_relative_error(
+    original, 
+    approximation,
+    **kwargs,
+):
+    """
+    Returns
+        The error relative to the norm of the original tensor.
+    """
+    with torch.no_grad():
+        return torch.norm(original-approximation, **kwargs) / torch.norm(original, **kwargs)
