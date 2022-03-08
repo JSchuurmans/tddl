841dbece818eec73fe28a34c14cef8327091bc41
JSchuurmans <jetzeschuurmans@gmail.com>
2022-03-08 10:09:58 +0100

    cifar10 loaders

--- src/tddl/data/loaders.py
+++ src/tddl/data/loaders.py
@@ -162,3 +162,95 @@ def fmnist_stratified_loaders(
     )
 
     return train_loader, valid_loader, test_loader
+
+
+def cifar10_stratified_loaders(
+    path: Path,
+    batch_size: int,
+    data_workers: int,
+    valid_size: int = 5000,
+    random_transform_training: bool = True,
+) -> Tuple[DataLoader,...]:
+    ...
+    '''
+        input:
+        - valid_size: total number of validation observations
+    '''
+
+    mean_CIFAR10 = [0.49139968, 0.48215841, 0.44653091]
+    std_CIFAR10 = [0.24703223, 0.24348513, 0.26158784]
+
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(
+            mean_CIFAR10,
+            std_CIFAR10,
+        ),
+    ])
+
+    transform_train = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(
+            mean_CIFAR10,
+            std_CIFAR10,
+        ),
+    ]) if random_transform_training else transform_test
+
+    dataset = datasets.CIFAR10(path, train=True, download=True)
+
+    num_train = len(dataset)
+    indices = list(range(num_train))
+
+    train_idx, valid_idx, _, _ = train_test_split(indices, 
+        dataset.targets, test_size=valid_size, 
+        stratify=dataset.targets, random_state=42,
+    )
+
+    train_sampler = SubsetRandomSampler(train_idx)
+    valid_sampler = SubsetRandomSampler(valid_idx)
+
+    train_dataset = datasets.CIFAR10(
+        root=path, train=True,
+        download=True, transform=transform_train,
+    )
+
+    valid_dataset = datasets.CIFAR10(
+        root=path, train=True,
+        download=True, transform=transform_test,
+    )
+
+    train_loader = DataLoader(
+        train_dataset, batch_size=batch_size, 
+        sampler=train_sampler,
+        num_workers=data_workers,
+    )
+    valid_loader = DataLoader(
+        valid_dataset, batch_size=batch_size, 
+        sampler=valid_sampler,
+        num_workers=data_workers,
+    )
+
+    test_dataset = datasets.CIFAR10(
+        path, train=False, transform=transform_test, download=True
+    )
+    test_loader = DataLoader(
+        test_dataset, 
+        batch_size=batch_size,
+        num_workers=data_workers,
+    )
+
+    return train_loader, valid_loader, test_loader
+
+
+def fetch_loaders(
+    dataset='fmnist',
+    **kwargs,
+):
+    loaders = {
+        'fmnist': fmnist_stratified_loaders,
+        'cifar10': cifar10_stratified_loaders,
+    }
+
+    return loaders[dataset](**kwargs)
